87449a87cb366ef51e96fc043c8aa92ec7cf6928
AvinashT31 <115381340+DakhaniUsman@users.noreply.github.com>
2024-09-16 16:16:54 +0530

    header done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,39 @@
+// mobile header
+
+
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+
+function HideSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
+
+function hide() {
+    sidebar.style.display = 'none';
+}
 
 // header scroll
 document.addEventListener('scroll', () => {
     const header = document.querySelector('header');
 
-    if(window.scrollY > 0){
+    if (window.scrollY > 0) {
         header.classList.add('scrolled');
     } else {
         header.classList.remove('scrolled');
     }
 });
 
+
+
 // automatic typing
 
-var typed = new Typed(".text" , {
-    strings :["Front End Developer" , "Web Developer", "Testor"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
+var typed = new Typed(".text", {
+    strings: ["Front End Developer", "Web Developer", "Testor"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
 });
-
-// mobile nav
-const hamMenu = document.querySelector('.ham-menu');
-
-const offScreenMenu = document.querySelector('.off-screen-menu');
-
-hamMenu.addEventListener('.click', () => {
-    hamMenu.classList.toggle('active');
-    offScreenMenu.classList.toggle('active');
-})
